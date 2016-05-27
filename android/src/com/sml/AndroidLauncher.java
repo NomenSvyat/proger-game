717cfbcc7ebd880efd024b3684d77195d936a138
Chernov Svyatoslav <schernov@smedialink.com>
2016-05-27 07:25:00 +0300

    init

---
+++ android/src/com/sml/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.sml;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sml.ProgerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new ProgerGame(), config);
+	}
+}
