b399ad2b4b801753e84917bee47687bf2fa4eb97
Plotnikov Timofey <tplotnikov@smedialink.com>
2016-05-31 15:20:33 +0300

    leader screen interface

---
+++ core/src/com/sml/utils/ILeaderScreen.java
@@ -0,0 +1,10 @@
+package com.sml.utils;
+
+/**
+ * Created on 31.05.16.
+ *
+ * @author Timofey Plotnikov <timofey.plot@gmail.com>
+ */
+public interface ILeaderScreen {
+    void showLeaderScreen(int score);
+}
